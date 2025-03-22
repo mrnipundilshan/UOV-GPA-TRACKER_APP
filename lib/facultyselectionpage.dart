274ef95c0ac00f99b866b0c9f7e37b7fa91849d1
Nipun Dilshan <139701467+mrnipundilshan@users.noreply.github.com>
2025-03-22 15:21:53 +0530

    Add faculty selection dropdown to FacultySelectionPage and update main app entry

--- lib/facultyselectionpage.dart
+++ lib/facultyselectionpage.dart
@@ -1,3 +1,5 @@
+import 'package:dropdown_button2/dropdown_button2.dart';
+
 import 'background/bg.dart';
 import 'package:flutter/material.dart';
 
@@ -6,6 +8,76 @@ class FacSelectionPage extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return const Background(child: Text("data"));
+    double width = MediaQuery.of(context).size.width;
+    double height = MediaQuery.of(context).size.height;
+    final List<String> Faculties = ['FAS', 'Tech', 'Bussiness Studies'];
+
+    String? selectedValue;
+
+    final _formKey = GlobalKey<FormState>();
+    return Scaffold(
+      body: Background(
+        child: Form(
+          key: _formKey,
+          child: Column(
+            mainAxisAlignment: MainAxisAlignment.center,
+            children: [
+              DropdownButtonFormField2<String>(
+                isExpanded: true,
+                decoration: InputDecoration(
+                  // Add Horizontal padding using menuItemStyleData.padding so it matches
+                  // the menu padding when button's width is not specified.
+                  contentPadding: const EdgeInsets.symmetric(vertical: 25),
+                  border: OutlineInputBorder(
+                    borderRadius: BorderRadius.circular(20),
+                  ),
+
+                  maintainHintHeight: true,
+                ),
+
+                hint: Text(
+                  'Select Your Faculty',
+                  style: TextStyle(fontSize: width * 0.07),
+                ),
+                items:
+                    Faculties.map(
+                      (item) => DropdownMenuItem<String>(
+                        value: item,
+                        child: Text(item, style: const TextStyle(fontSize: 14)),
+                      ),
+                    ).toList(),
+                validator: (value) {
+                  if (value == null) {
+                    return 'Please select Faculty.';
+                  }
+                  return null;
+                },
+                onChanged: (value) {
+                  //Do something when selected item is changed.
+                },
+                onSaved: (value) {
+                  selectedValue = value.toString();
+                },
+                buttonStyleData: const ButtonStyleData(
+                  padding: EdgeInsets.only(right: 10),
+                ),
+                iconStyleData: const IconStyleData(
+                  icon: Icon(Icons.arrow_drop_down, color: Colors.white),
+                  iconSize: 24,
+                ),
+                dropdownStyleData: DropdownStyleData(
+                  decoration: BoxDecoration(
+                    borderRadius: BorderRadius.circular(15),
+                  ),
+                ),
+                menuItemStyleData: const MenuItemStyleData(
+                  padding: EdgeInsets.symmetric(horizontal: 16),
+                ),
+              ),
+            ],
+          ),
+        ),
+      ),
+    );
   }
 }
--- lib/main.dart
+++ lib/main.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'package:newgpaapp/facultyselectionpage.dart';
 import 'package:newgpaapp/homepage.dart';
 
 void main() {
@@ -18,7 +19,7 @@ class MyApp extends StatelessWidget {
           brightness: Brightness.dark,
           primaryColor: Colors.cyanAccent,
         ),
-        home: Homepage(),
+        home: FacSelectionPage(),
       ),
     );
   }
