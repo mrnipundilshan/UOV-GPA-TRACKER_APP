ffc9de817e824a389f405c9b3f8868aff4f1d537
Nipun Dilshan <139701467+mrnipundilshan@users.noreply.github.com>
2025-03-23 18:21:12 +0530

    Add course selection dropdown to FacultySelectionPage and update UI layout

--- lib/facultyselectionpage.dart
+++ lib/facultyselectionpage.dart
@@ -12,71 +12,149 @@ class FacSelectionPage extends StatefulWidget {
 
 class _FacSelectionPageState extends State<FacSelectionPage> {
   final List<String> faculties = <String>['FAS', 'Tech', 'Bussiness Studies'];
+  final Map<String, List<String>> courses = {
+    'FAS': ['IT', 'BIO', 'AMC', 'CS'],
+    'Tech': ['Engineering', 'Computer Science', 'Electronics'],
+    'Bussiness Studies': ['Marketing', 'Finance', 'Management'],
+  };
+
   String? selectedFac;
+  String? selectedCourse;
+  List<String> availableCourses = [];
 
   @override
   Widget build(BuildContext context) {
+    double height = MediaQuery.of(context).size.height;
+    double width = MediaQuery.of(context).size.width;
     return Scaffold(
       body: Background(
         child: Column(
-          mainAxisAlignment: MainAxisAlignment.center,
+          mainAxisAlignment: MainAxisAlignment.spaceBetween,
           children: [
-            Center(
-              child: DropdownButton2<String>(
-                underline: SizedBox.shrink(),
-                hint: Text(
-                  'Select Faculty',
-                  style: TextStyle(fontSize: 16, color: Colors.white),
-                  overflow: TextOverflow.ellipsis,
-                ),
+            SizedBox(height: height * 0.05),
+            Column(
+              children: [
+                DropdownButton2<String>(
+                  underline: SizedBox.shrink(),
+                  hint: Text(
+                    'Select Faculty',
+                    style: TextStyle(fontSize: 16, color: Colors.white),
+                    overflow: TextOverflow.ellipsis,
+                  ),
 
-                items:
-                    faculties
-                        .map(
-                          (String value) => DropdownMenuItem<String>(
-                            value: value,
-                            child: Text(
-                              value,
-                              style: TextStyle(
-                                fontSize: 16,
-                                color: Colors.white,
+                  items:
+                      faculties
+                          .map(
+                            (String value) => DropdownMenuItem<String>(
+                              value: value,
+                              child: Text(
+                                value,
+                                style: TextStyle(
+                                  fontSize: 16,
+                                  color: Colors.white,
+                                ),
+                                overflow: TextOverflow.ellipsis,
                               ),
-                              overflow: TextOverflow.ellipsis,
                             ),
-                          ),
-                        )
-                        .toList(),
-                value: selectedFac,
-                onChanged: (String? newValue) {
-                  setState(() {
-                    selectedFac = newValue!;
-                  });
-                },
-                buttonStyleData: ButtonStyleData(
-                  decoration: BoxDecoration(
-                    borderRadius: BorderRadius.circular(8),
-                    color: const Color.fromARGB(255, 45, 100, 107),
+                          )
+                          .toList(),
+                  value: selectedFac,
+                  onChanged: (String? newValue) {
+                    setState(() {
+                      selectedFac = newValue!;
+                      selectedCourse = null; // Reset course selection
+                      availableCourses =
+                          courses[selectedFac] ??
+                          []; // Update available courses
+                    });
+                  },
+                  buttonStyleData: ButtonStyleData(
+                    decoration: BoxDecoration(
+                      borderRadius: BorderRadius.circular(8),
+                      color: const Color.fromARGB(255, 45, 100, 107),
+                    ),
+                    padding: const EdgeInsets.symmetric(horizontal: 16),
+                    height: height * 0.07,
+                    width: width * 0.5,
+                    elevation: 1,
                   ),
-                  padding: const EdgeInsets.symmetric(horizontal: 16),
-                  height: 60,
-                  elevation: 1,
+
+                  iconStyleData: const IconStyleData(
+                    icon: Icon(Icons.arrow_forward_ios_outlined),
+                    iconSize: 14,
+                    iconDisabledColor: Colors.white,
+                    iconEnabledColor: Colors.white,
+                  ),
+                  dropdownStyleData: DropdownStyleData(
+                    decoration: BoxDecoration(
+                      borderRadius: BorderRadius.circular(14),
+                      color: const Color.fromARGB(255, 48, 131, 151),
+                    ),
+                    offset: const Offset(0, 0),
+                  ),
                 ),
 
-                iconStyleData: const IconStyleData(
-                  icon: Icon(Icons.arrow_forward_ios_outlined),
-                  iconSize: 14,
-                  iconDisabledColor: Colors.white,
-                  iconEnabledColor: Colors.white,
-                ),
-                dropdownStyleData: DropdownStyleData(
-                  decoration: BoxDecoration(
-                    borderRadius: BorderRadius.circular(14),
-                    color: const Color.fromARGB(255, 48, 131, 151),
+                SizedBox(height: height * 0.05),
+
+                DropdownButton2<String>(
+                  underline: SizedBox.shrink(),
+                  hint: Text(
+                    'Select Course',
+                    style: TextStyle(fontSize: 16, color: Colors.white),
+                    overflow: TextOverflow.ellipsis,
                   ),
-                  offset: const Offset(0, 0),
+
+                  items:
+                      availableCourses
+                          .map(
+                            (String value) => DropdownMenuItem<String>(
+                              value: value,
+                              child: Text(
+                                value,
+                                style: TextStyle(
+                                  fontSize: 16,
+                                  color: Colors.white,
+                                ),
+                                overflow: TextOverflow.ellipsis,
+                              ),
+                            ),
+                          )
+                          .toList(),
+                  value: selectedCourse,
+                  onChanged: (String? newValue) {
+                    setState(() {
+                      selectedCourse = newValue!;
+                    });
+                  },
+                  buttonStyleData: ButtonStyleData(
+                    decoration: BoxDecoration(
+                      borderRadius: BorderRadius.circular(8),
+                      color: const Color.fromARGB(255, 45, 100, 107),
+                    ),
+                    padding: const EdgeInsets.symmetric(horizontal: 16),
+                    height: height * 0.07,
+                    width: width * 0.5,
+                    elevation: 1,
+                  ),
+
+                  iconStyleData: const IconStyleData(
+                    icon: Icon(Icons.arrow_forward_ios_outlined),
+                    iconSize: 14,
+                    iconDisabledColor: Colors.white,
+                    iconEnabledColor: Colors.white,
+                  ),
+                  dropdownStyleData: DropdownStyleData(
+                    decoration: BoxDecoration(
+                      borderRadius: BorderRadius.circular(14),
+                      color: const Color.fromARGB(255, 48, 131, 151),
+                    ),
+                    offset: const Offset(0, 0),
+                  ),
                 ),
-              ),
+              ],
             ),
+
+            SizedBox(height: height * 0.05),
           ],
         ),
       ),
